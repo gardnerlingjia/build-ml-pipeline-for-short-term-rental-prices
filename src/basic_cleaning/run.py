a3cc421a89fbef6658d0771a5a1340053ee09811
Lingjia Gardner <lingjia.gardner@cariad.technology>
2026-04-23 12:52:28 +0200

    Filter listings outside NYC boundaries

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+"""
+Performs basic cleaning on the data and saves the results in W&B
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    # Download input artifact
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    # Read data
+    df = pd.read_csv(artifact_local_path)
+
+    # Basic cleaning
+    df = df.drop_duplicates()
+    df = df.dropna(subset=["price"])
+    df = df[df["price"].between(args.min_price, args.max_price)]
+
+
+    # Filter rows outside NYC boundaries
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    logger.info("Cleaned data has %s rows and %s columns", *df.shape)
+
+    # Save cleaned data
+    df.to_csv("clean_sample.csv", index=False)
+
+    # Log the cleaned dataset as a new artifact
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(filename)
+    run.log_artifact(artifact)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="This step cleans the data")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="Fully-qualified name for the input artifact to clean",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="Name for the output cleaned artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="Type of the output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="Description of the output artifact",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="Minimum price allowed for the cleaned dataset",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help="Maximum price allowed for the cleaned dataset",
+        required=True,
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
